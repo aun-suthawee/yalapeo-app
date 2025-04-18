63b439709757cac0ecfcbebc6d68edac0e27108d
aunsuthawee <aunderu@hotmail.com>
2025-04-18 14:05:10 +0700

    added description about facebook video url

--- ~app/Modules/Video/Resources/views/admin/create.blade.php
+++ ~app/Modules/Video/Resources/views/admin/create.blade.php
@@ -1,49 +1,65 @@
 @extends('admin::layouts.master')
 
 @section('app-content')
-		<div class="row justify-content-center">
-				<div class="col-11">
-						<x-alert-error-message />
+    <div class="row justify-content-center">
+        <div class="col-11">
+            <x-alert-error-message />
 
-						<form action="{{ route('admin.video.store') }}" method="POST" enctype="multipart/form-data">
-								@csrf
-								<div class="tile">
-										<h3 class="tile-title">สร้างใหม่</h3>
-										<div class="tile-body">
-												<div class="form-row">
-														<div class="form-group col-md-7">
-																<label>ที่อยู่วิดิโอ <span class="badge badge-danger">YouTube</span> <span class="badge badge-primary">Facebook</span></label>
-																<input type="text" class="form-control @error('url') is-invalid @enderror" name="url" value="{{ old('url') }}" placeholder="ex. https://www.youtube.com/watch?v=6Oo5trPKpOo">
-																<x-error-message title="url" />
-														</div>
-														<div class="form-group col-md-5">
-																<label>หัวข้อ</label>
-																<input type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}" placeholder="ระบุหัวข้อ" required>
-																<x-error-message title="title" />
-														</div>
-												</div>
-												<div class="form-row">
-														<div class="form-group col-md-12">
-																<label>รายละเอียด</label>
-																<textarea class="form-control" name="detail" id="detail" placeholder="ระบุรายละเอียด..."></textarea>
+            <form action="{{ route('admin.video.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="tile">
+                    <h3 class="tile-title">สร้างใหม่</h3>
+                    <div class="tile-body">
+                        <div class="form-row">
+                            <div class="form-group col-md-7">
+                                <label>ที่อยู่วิดิโอ <span class="badge badge-danger">YouTube</span> <span
+                                        class="badge badge-primary">Facebook</span></label>
+                                <input type="text" class="form-control @error('url') is-invalid @enderror" name="url"
+                                    value="{{ old('url') }}"
+                                    placeholder="ex. https://www.youtube.com/watch?v=6Oo5trPKpOo">
+                                <small class="form-text text-muted">
+                                    สำหรับวิดีโอจาก
+                                    <b>Facebook</b> เพื่อให้สามารถบันทึกข้อมูลลงสู่ระบบได้ให้ทำตามขั้นตอนดังนี้<br>
+                                    1. คัดลอกตัวเลข ID ตรงท้ายลิงก์ (ตัวเลขหลังคำว่า "videos/")<br>
+                                    2. แล้วใส่ตัวเลขนั้นหลัง
+                                    "watch?v=" แทน<br><br>
+										
+										<b>ตัวอย่าง:</b><br>
+										ลิงก์เดิมที่บันทึกมาจากเพจ ศธจ.ยะลา: https://www.facebook.com/yalaedu/videos/1367499458026596<br>
+										ลิงก์ใหม่ เพื่อที่จะสามารถบันทึกได้: https://www.facebook.com/watch?v=1367499458026596
+								</small>
+                                <x-error-message title="url"
+                                    />
+                            </div>
+                            <div class="form-group col-md-5">
+                                <label>หัวข้อ</label>
+                                <input type="text" class="form-control @error('title') is-invalid @enderror"
+                                    name="title" value="{{ old('title') }}" placeholder="ระบุหัวข้อ" required>
+                                <x-error-message title="title" />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-12">
+                                <label>รายละเอียด</label>
+                                <textarea class="form-control" name="detail" id="detail" placeholder="ระบุรายละเอียด..."></textarea>
                                 {!! ckeditor_advanced_url('detail') !!}
-														</div>
-												</div>
-										</div>
-										<div class="tile-footer">
-												<button type="submit" class="btn btn-primary">
-														<i class="fas fa-check-circle fa-fw"></i>บันทึก
-												</button>
-												<button type="reset" class="btn btn-light">
-														<i class="fa fa-times-circle fa-fw"></i>ยกเลิก
-												</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tile-footer">
+                        <button type="submit" class="btn btn-primary">
+                            <i class="fas fa-check-circle fa-fw"></i>บันทึก
+                        </button>
+                        <button type="reset" class="btn btn-light">
+                            <i class="fa fa-times-circle fa-fw"></i>ยกเลิก
+                        </button>
 
-										</div>
-								</div>
-						</form>
-				</div>
-		</div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
 @section('script-content')
-		<script type="text/javascript"></script>
+    <script type="text/javascript"></script>
 @endsection
